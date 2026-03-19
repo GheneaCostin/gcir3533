39bd1206411d5cdfe4965f19a84c0d4557de0498
Costin <ghenea.costin@gmail.com>
2026-03-19 20:36:41 +0200

    First Commit

---
+++ src/main/java/drinkshop/reports/DailyReportService.java
@@ -0,0 +1,25 @@
+package drinkshop.reports;
+
+import drinkshop.domain.Order;
+import drinkshop.repository.Repository;
+
+
+
+public class DailyReportService {
+    private Repository<Integer, Order> repo;
+
+    public DailyReportService(Repository<Integer, Order> repo) {
+        this.repo = repo;
+    }
+
+    public double getTotalRevenue() {
+        return repo.findAll().st    ream().mapToDouble(Order::getTotal).sum();
+    }
+
+    public int getTotalOrders() {
+//        List<Order> orders = StreamSupport.stream(repo.findAll().spliterator(), false)
+//                .collect(Collectors.toList());
+
+        return repo.findAll().size();
+    }
+}
